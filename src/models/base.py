42c385d1004c2feeabd7e4f74d739feb2908b5e3
MajkelDcember <slim100010@gmail.com>
2025-01-20 17:06:10 +0100

    added random blocks

--- src/models/base.py
+++ src/models/base.py
@@ -110,13 +110,13 @@ class Block(nn.Module):
             self.attn_block = LightEncoderBlock(
                 model_dim=config.n_embd,
                 block_dim=config.block_dim,
-                n_heads=config.n_heads,
+                n_head=config.n_head,
                 dropout_rate=config.dropout,
             )
         else:
             self.attn_block = EncoderBlock(
                 model_dim=config.n_embd,
-                n_heads=config.n_heads,
+                n_head=config.n_head,
                 dropout_rate=config.dropout,
             )
 
