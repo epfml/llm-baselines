7a0764a358595a59df4876322cd369ca8420a895
MajkelDcember <slim100010@gmail.com>
2025-01-23 17:23:40 +0100

    biases, layer notm and better initialization

--- src/models/randatt/blocks.py
+++ src/models/randatt/blocks.py
@@ -9,17 +9,7 @@ from .attention import SelfAttention, CrossAttention, RandomBlockSelfAttention, 
 from .tools import causal_mask, alibi_shift
 
 from torch.nn import functional as F
-
-class LayerNorm(nn.Module):
-    """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
-
-    def __init__(self, ndim, bias):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
-
-    def forward(self, input):
-        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+from src.models.base.py import LayerNorm
 
 
 class MLP(nn.Module):
