8b8732a3173ae04b2df71dd68d5aa39fa2daec33
MajkelDcember <slim100010@gmail.com>
2025-02-20 11:35:48 +0100

    fix for spda flop counting

--- src/optim/base.py
+++ src/optim/base.py
@@ -21,19 +21,15 @@ def profile_fvcore_flops(model, sequence_length, vocab_size, device):
     dummy_input = torch.randint(0, vocab_size, (1, sequence_length), device=device)
     dummy_target = torch.zeros_like(dummy_input)
 
-    # Call the model forward pass with `force_no_flash=True`
-    outputs = model(dummy_input, dummy_target, force_no_flash=True)
+    flop_analyzer = FlopCountAnalysis(model, (dummy_input, dummy_target))
+    flop_analyzer.set_op_handle(CausalSelfAttention, attention_flop_counter)
 
-    # Measure flops for this specific forward call
-    flops, _ = flop_count(
-        model,
-        (dummy_input, dummy_target),
-        supported_ops={CausalSelfAttention: attention_flop_counter},
-    )
-
-    total_flops = sum(flops.values())
+    total_flops = flop_analyzer.total()
     print(f"[FvCore Profiling] Total FLOPs (per forward pass): {total_flops:,}")
+    print(flop_count_table(flop_analyzer))
+
     return total_flops
+
 
 
 
